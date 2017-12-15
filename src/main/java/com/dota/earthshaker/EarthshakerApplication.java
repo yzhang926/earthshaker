89593d03a41ea959b9d5fd8042281b4a66a2dc51
Marco <lwh9072@gmail.com>
2017-12-14 20:13:00 -0500

    project init

---
+++ src/main/java/com/dota/earthshaker/EarthshakerApplication.java
@@ -0,0 +1,12 @@
+package com.dota.earthshaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthshakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthshakerApplication.class, args);
+	}
+}
